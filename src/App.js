4ea1591c9a509f1ebad489d831912a4afb3916cc
valdeci <moura97.valdeci@gmail.com>
2022-07-13 17:25:24 -0300

    feat: create global style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './App.css';
 import Provider from './context/Provider';
 import Routes from './pages';
+import GlobalStyle from './styles';
 
 function App() {
   return (
     <Provider>
       <Routes />
+      <GlobalStyle />
     </Provider>
   );
 }
