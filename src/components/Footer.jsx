0f5f1a32da80e6d65dbbde0c6633de3999422478
valdeci <moura97.valdeci@gmail.com>
2022-07-27 14:20:29 -0300

    feat: create footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,26 +3,27 @@ import AppContext from '../context/ContextAPI';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
-import './Footer.css';
+
+import * as S from '../styles/footer';
 
 export default function Footer() {
   const { goesTo } = useContext(AppContext);
 
   return (
-    <footer data-testid="footer">
-      <button type="button" onClick={ () => goesTo('bebidas') }>
-        <img
+    <S.Container data-testid="footer">
+      <S.Button type="button" onClick={ () => goesTo('bebidas') }>
+        <S.Image
           src={ drinkIcon }
           alt="Taça de vidro com limão na borda"
           data-testid="drinks-bottom-btn"
         />
-      </button>
-      <button type="button" onClick={ () => goesTo('explorar') }>
-        <img src={ exploreIcon } alt="Bússola" data-testid="explore-bottom-btn" />
-      </button>
-      <button type="button" onClick={ () => goesTo('comidas') }>
-        <img src={ mealIcon } alt="Garfo e faca" data-testid="food-bottom-btn" />
-      </button>
-    </footer>
+      </S.Button>
+      <S.Button type="button" onClick={ () => goesTo('explorar') }>
+        <S.Image src={ exploreIcon } alt="Bússola" data-testid="explore-bottom-btn" />
+      </S.Button>
+      <S.Button type="button" onClick={ () => goesTo('comidas') }>
+        <S.Image src={ mealIcon } alt="Garfo e faca" data-testid="food-bottom-btn" />
+      </S.Button>
+    </S.Container>
   );
 }
---
+++ src/styles/footer.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.footer`
+  display: flex;
+  height: 5rem;
+  justify-content: space-around;
+  margin-top: 0.5rem;
+`;
+
+export const Button = styled.button`
+  border: none;
+`;
+
+export const Image = styled.img`
+  width: 3rem;
+`;
